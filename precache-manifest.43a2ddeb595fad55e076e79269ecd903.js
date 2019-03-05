efb1c083ae2c468df15d7897a12f906ed34e4755
Abigail Swarth <abigailswarth@gmail.com>
2019-03-04 22:59:20 -0500

    Updates

---
+++ precache-manifest.43a2ddeb595fad55e076e79269ecd903.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "578d6469e54b78dd1e3af71aa89dd76f",
+    "url": "/googlebooks-chingu/static/media/book.578d6469.jpg"
+  },
+  {
+    "revision": "6c918b495f41d577422f",
+    "url": "/googlebooks-chingu/static/js/runtime~main.6c918b49.js"
+  },
+  {
+    "revision": "979f9d0392f353fbdab5",
+    "url": "/googlebooks-chingu/static/js/main.979f9d03.chunk.js"
+  },
+  {
+    "revision": "9f868d30a58aae4fa46d",
+    "url": "/googlebooks-chingu/static/js/2.9f868d30.chunk.js"
+  },
+  {
+    "revision": "979f9d0392f353fbdab5",
+    "url": "/googlebooks-chingu/static/css/main.7194d27e.chunk.css"
+  },
+  {
+    "revision": "e71a217ca3b3b4b769cea3baac4aa23f",
+    "url": "/googlebooks-chingu/index.html"
+  }
+];
